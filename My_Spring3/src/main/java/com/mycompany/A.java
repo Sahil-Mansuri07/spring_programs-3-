ff84029d127907c72d304ef40fc84681c1662cdd
Sahil-Mansuri07 <sahilmansuri0021@gmail.com>
2025-07-11 16:31:43 +0530

    third spring program

---
+++ My_Spring3/src/main/java/com/mycompany/A.java
@@ -0,0 +1,21 @@
+package com.mycompany;
+
+public class A {
+
+	Integer x;
+	B ref;
+	public Integer getX() {
+		return x;
+	}
+	public void setX(Integer x) {
+		this.x = x;
+	}
+	public B getRef() {
+		return ref;
+	}
+	public void setRef(B ref) {
+		this.ref = ref;
+	}
+	
+	
+}
---
+++ My_Spring3/src/main/java/com/mycompany/B.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+
+public class B {
+
+	Integer y;
+
+	public Integer getY() {
+		return y;
+	}
+
+	public void setY(Integer y) {
+		this.y = y;
+	}
+	
+}
